426a629263d95230351f0e071effcc9e68531315
Johnathan Niziol <jniziol@gmail.com>
2019-01-17 22:56:27 -0600

    Updates state branch

--- src/Restaurant.js
+++ src/Restaurant.js
@@ -10,6 +10,8 @@ class Restaurant extends Component {
           <div><span>Name:</span> {this.props.name}</div>
           <div><span>Rating:</span> {this.props.rating}</div>
           <a href="#">Reviews</a>
+          <span onClick={()=> this.props.increaseRating(this.props.name)}>+</span>
+          <span onClick={() => this.props.decreaseRating(this.props.name)}>-</span>
         </div>
       </div>
     )
